d95b0c4636466672a70a16f7108b722277d5551c
Katsutoshi Seki <sekika@users.noreply.github.com>
2015-10-01 15:59:52 +0900

    minor edit

--- Install.sh
+++ Install.sh
@@ -92,11 +92,11 @@ echo "Downloading \c"
 
 for i in leasqr.m dfdp.m cpiv_bard.m; do
   echo $i"... \c"
-  wget -q -O $loadpath/$i "$package""$i""?format=raw"
+  $wget -q -O $loadpath/$i "$package""$i""?format=raw"
 done
 for i in __dfdp__.m __lm_svd__.m __plot_cmds__.m __do_user_interaction__.m; do
   echo $i"... \c"
-  wget -q -O $loadpath/$i "$package"private/"$i""?format=raw"
+  $wget -q -O $loadpath/$i "$package"private/"$i""?format=raw"
 done
 echo "finished."
 
